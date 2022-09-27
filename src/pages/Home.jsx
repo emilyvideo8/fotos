99e0263c517b48003a29eb2f28a4fb03aa3a7a05
emilyvideo8 <emilyvideo8@gmail.com>
2022-09-27 15:26:56 -0400

    l

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import Background from '../images/backgroundHome.jpg'
+import Background1 from '../images/background1.jpg'
 import { Link } from 'react-router-dom'
+
 
 const Home = () => {
   return (
 
 
     <>
-
-    <div className="home__container">
+    <div className="home">
+      <div className="home__container">
         <h1> esta pagina contiene mujeres sin ropa estas seguro que deseas continuar ??? </h1>
-        <img src={Background} alt="background" />
-        <button><Link to='videos' > si</Link></button>
-        <button><a href='https://google.com'>no</a></button>
+        <img src={Background1} alt="background" />
+        <button><Link to='videos' > Ver Videos </Link></button>
+        <button><a href='https://google.com'> Salir de la Pagina </a></button>
+      </div>
     </div>
-
     </>
   )
 }
