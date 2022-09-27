1ed1d7a2c464e2db21d1c4573c04182b865917ad
emilyvideo8 <emilyvideo8@gmail.com>
2022-09-27 13:37:20 -0400

    CNAME added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from 'react';
 import Escuela from './escuela.jpg'
 import './App.css';
 import Card from './components/Card';
 
 function App() {
+
+  const [displayNone, setDisplayNone] = useState(false);
+
   return (
     // <div className='card'>
     //   <div className='image__container'>
@@ -23,6 +27,13 @@ function App() {
         titulo='en estos videos me dan duro por atras'
         anuncio='http://lyksoomu.com/OPFn'
         pago='https://buy.stripe.com/fZeg1OcQv0mi5OwbIJ' />
+        <div className='popup'>
+          <div className={`${ displayNone ? 'popup__container' : 'display__none' }`}> 
+            <h1>este contenido es para mayores de edad deseas continuar ??? </h1>
+            <button onClick={()=> setDisplayNone(true)}> si</button>
+            <button>no</button>
+           </div>
+        </div>
     </>
 
   );
