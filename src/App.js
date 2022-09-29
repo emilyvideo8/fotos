391323ee83f95a562ff03f2379bf5d002896157e
emilyvideo8 <emilyvideo8@gmail.com>
2022-09-28 23:07:30 -0400

    0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import './App.css';
-import Home from './pages/Home';
 import Videos from './pages/Videos';
 import Fotos from './pages/Fotos';
 
@@ -13,9 +12,7 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path='home' element={<Home />} />
-        <Route path='videos' element={<Videos />} />
+        <Route index element={<Videos />} />
         <Route path='fotos' element={<Fotos />} />
       </Routes>
     </HashRouter>
